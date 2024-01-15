b2cddefd2e02efbd3f847a15f8c9ac20ca499c7b
vishnu <vishnugawade90@gmail.com>
2024-01-15 17:35:38 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes/user-routes.js';
 import cors from 'cors';
-import stripe from 'stripe';
 //const stripe = require('stripe')
 
 const app = express();
@@ -10,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/user', router)
 mongoose.connect("mongodb+srv://vishnugawade795:fqrkg48K9pxUEFkW@FlixitApplication.7ugvftd.mongodb.net/?retryWrites=true&w=majority")
-  .then(() => app.listen(5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .then(() => console.log("Connected to databse and listening on the port 5000"))
   .catch((err) => console.log(err));
 
